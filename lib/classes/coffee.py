ca8a07c02d3793cf9085e493d98cb8ddae1d949d
Eli Dawson <edawson@alumni.berklee.edu>
2023-05-31 22:45:35 -0400

    Completed Lab

--- lib/classes/coffee.py
+++ lib/classes/coffee.py
@@ -1,17 +1,36 @@
 class Coffee:
+
     def __init__(self, name):
         self.name = name
-        
+        self._orders = []
+        self._customers = []
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if hasattr(self, 'name'):
+            raise Exception
+        elif isinstance(name, str):
+            self._name = name
+
     def orders(self, new_order=None):
         from classes.order import Order
-        pass
+        if new_order and isinstance(new_order, Order):
+            self._orders.append(new_order)
+        return self._orders
     
     def customers(self, new_customer=None):
         from classes.customer import Customer
-        pass
-    
+        if new_customer and isinstance(new_customer, Customer) and new_customer not in self._customers:
+        # if new_customer not in self._customers and isinstance(new_customer, Customer)
+            self._customers.append(new_customer)
+        return self._customers
+        
     def num_orders(self):
-        pass
+        return len(self._orders)
     
     def average_price(self):
-        pass
+        return sum([order.price for order in self._orders]) / len(self._orders)
